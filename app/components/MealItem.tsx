a948c2b5e4a6f86212a8cf2135218c2d45f5538d
Easykatka <secondjump@bk.ru>
2021-07-20 23:30:20 +0300

    24 meal item

---
+++ app/components/MealItem.tsx
@@ -0,0 +1,78 @@
+import {
+    ImageBackground,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from "react-native";
+import React from "react";
+import {IMeal} from "../models/meal";
+
+interface Props {
+    item: IMeal;
+    onSelect: () => void;
+}
+
+export const MealItem: React.ComponentType<Props> = ({item, onSelect}) => {
+
+    return <View style={styles.mealItem}>
+        <TouchableOpacity onPress={onSelect}>
+            <View>
+                <View style={[styles.mealRow, styles.header]}>
+                    <ImageBackground source={{uri: item.imageUrl}}
+                                     style={styles.image}>
+                        <Text numberOfLines={1}
+                              style={styles.title}>{item.title}</Text>
+                    </ImageBackground>
+                </View>
+                <View style={[styles.mealRow, styles.detail]}>
+                    <Text>{item.duration}m</Text>
+                    <Text>{item.complexity.toUpperCase()}</Text>
+                    <Text>{item.affordability.toUpperCase()}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        color: 'white',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        textAlign: 'center',
+        paddingVertical:5,
+        paddingHorizontal:15,
+
+
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'flex-end',
+
+
+    },
+    detail: {
+        justifyContent: 'space-between',
+
+        height:'15%',
+        paddingVertical:5,
+        paddingHorizontal:15,
+    },
+    header: {
+        height: '85%'
+    },
+    mealItem: {
+        marginVertical: 10,
+        height: 200,
+        width: '100%',
+        backgroundColor: 'rgba(197,193,193,0.89)',
+        borderRadius:15,overflow:'hidden'
+    },
+    mealRow: {
+        flexDirection: 'row',
+    }
+})
--- app/screens/CategoryMeals.tsx
+++ app/screens/CategoryMeals.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import {NavigationStackScreenComponent} from "react-navigation-stack";
 import {CATEGORIES, MEALS} from "../data/dummy-data";
 import {IMeal} from "../models/meal";
+import {MealItem} from "../components/MealItem";
 
 export const CategoryMeals: NavigationStackScreenComponent = ({navigation}) => {
 
     const categoryId = navigation.getParam('categoryId')
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.includes(categoryId))
-    const renderMealItem = ({item}: { item: IMeal }) => <View><Text>{item.title}</Text></View>
+    const onSelect = () => {
+    }
+
+
+    const renderMealItem = ({item}: { item: IMeal }) =>
+
+        <MealItem item={item} onSelect={onSelect}/>
 
 
     return (
         <View style={styles.screen}>
-            <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id}
+            <FlatList data={displayedMeals} style={{width: '90%'}}
+                      keyExtractor={(item, index) => item.id}
                       renderItem={renderMealItem}/>
 
         </View>
